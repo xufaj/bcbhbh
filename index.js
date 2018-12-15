182094917986c54dd4a78066a024d71d3ca9d0ed
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-15 11:07:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -164,23 +164,5 @@ client.on('ready', () => {
   
   });
   
-  
-  client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
  
-
 client.login(process.env.TOKEN);
